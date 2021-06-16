a06d2a56cb3a846e2f4bdb5cb0aeff9299a8316a
Aika8 <aigerimt922@gmail.com>
2021-06-16 21:30:25 +0600

    initial commit

---
+++ src/test/java/com/example/eureka_task/EurekaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
